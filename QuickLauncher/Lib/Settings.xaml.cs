5a78061232b0624e72d8a3cd2dfdb0c82912bffb
SHHIKA <77447533+SHHIKA@users.noreply.github.com>
2023-08-01 14:36:23 +0900

    リファクタリング

--- QuickLauncher/Lib/Settings.xaml.cs
+++ QuickLauncher/Lib/Settings.xaml.cs
@@ -60,7 +60,7 @@ namespace QuickLauncher.Lib
             Activate();
         }
 
-        private void Window_Deactivated(object sender, System.EventArgs e) => Hide();
+        private void Window_Deactivated(object sender, EventArgs e) => Hide();
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
