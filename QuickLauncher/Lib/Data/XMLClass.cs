153a303e5b269937f84a32acc633fd167fc8f0c0
shika <77447533+SHHIKA@users.noreply.github.com>
2023-08-02 16:51:41 +0900

    XML変換クラスを作成 動作確認済み

---
+++ QuickLauncher/Lib/Data/XMLClass.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace QuickLauncher.Lib.Data
+{
+    public class XMLClass
+    {
+        public static void SaveData<T>(T data, string fileName)
+        {
+            string xml = SaveToString(data);
+
+            File.WriteAllText(fileName, xml);
+        }
+
+        public static T? LoadData<T>(string fileName) => LoadFromString<T>(File.ReadAllText(fileName));
+
+        public static string SaveToString<T>(T control)
+        {
+            var writer = new StringWriter();
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(writer, control);
+            
+            return writer.ToString();
+        }
+
+        public static T? LoadFromString<T>(string xml)
+        {
+            var serializer = new XmlSerializer(typeof(T));
+            return (T?)serializer.Deserialize(new StringReader(xml));
+        }
+    }
+}
